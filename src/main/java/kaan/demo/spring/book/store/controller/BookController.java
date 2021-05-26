46aefad87fbccb894708a672b75129b2d7cb5372
Kaan Bobac <bobackaan@gmail.com>
2021-05-26 15:22:34 +0300

    styles are added for table

--- src/main/java/kaan/demo/spring/book/store/controller/BookController.java
+++ src/main/java/kaan/demo/spring/book/store/controller/BookController.java
@@ -1,6 +1,7 @@
 package kaan.demo.spring.book.store.controller;
 
 import java.util.Comparator;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,12 +13,44 @@ import kaan.demo.spring.book.store.service.CsvLoader;
 
 @Controller
 public class BookController {
+	private boolean amountFlag = true;
+	private boolean idFlag = true;
 	@Autowired
 	private CsvLoader csvLoader;
 	@RequestMapping("/")
 	private String  queryAllBooks(Model model) {
 		 model.addAttribute("books", csvLoader.queryAll());
 		 model.addAttribute("byAmount", Comparator.comparing(Book::getAmount));
+		 return "books";
+	}
+	@RequestMapping("/sortbyAmount")
+	private String  sortBooksByAmount(Model model) {
+		List<Book> books = csvLoader.queryAll();
+		if(amountFlag) {
+			books.sort(Comparator.comparing(Book::getAmount));
+			amountFlag = false;
+		}
+			
+		else {
+			books.sort(Comparator.comparing(Book::getAmount).reversed());
+			amountFlag = true;
+		}
+		 model.addAttribute("books", books);
+		 return "books";
+	}
+	@RequestMapping("/sortbyId")
+	private String  sortBooksById(Model model) {
+		List<Book> books = csvLoader.queryAll();
+		if(idFlag) {
+			books.sort(Comparator.comparing(Book::getId));
+			idFlag = false;
+		}
+			
+		else {
+			books.sort(Comparator.comparing(Book::getId).reversed());
+			idFlag = true;
+		}
+		 model.addAttribute("books", books);
 		 return "books";
 	}
 }
